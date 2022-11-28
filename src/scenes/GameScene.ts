19626b78c081565e067ac616c11bea74cbe3ca67
Byron.C <byronrwcor@protonmail.com>
2022-11-27 21:01:36 -0800

    funny jump and fixed running

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -79,7 +79,11 @@ class GameScene extends Scene {
     // Player jumping
     if (this.cursors.up.isDown && this.player.body.blocked.down) {
       this.player.body.setVelocityY(-600);
-      this.player.play('player-jump');
+    }
+    if (this.player.body.blocked.down) {
+      this.player.play('player-run', true);
+    } else {
+      this.player.play('player-jump', true);
     }
   }
 }
