f7d5f2d6d1760d9dc0d11ec5a1dc0a8153352bb3
Kirk Morris <worker.kirk@gmail.com>
2022-11-27 20:38:07 -0800

    Adds static brick spawning + brick destroying.

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,4 +1,4 @@
-import { Scene, GameObjects, Display, Sound } from "phaser";
+import { Scene, GameObjects, Display, Sound, Math as pMath } from "phaser";
 
 class GameScene extends Scene {
   private baddy!: any;
@@ -7,6 +7,7 @@ class GameScene extends Scene {
   private player!: any;
   private platform!: GameObjects.Rectangle;
   private cursors!: any;
+  private activeBricks: any[] = [];
 
   constructor() {
     super('scene-game');
@@ -28,7 +29,6 @@ class GameScene extends Scene {
     this.baddy.body.setSize(32, 46);
 
     this.staticTile.mask = new Display.Masks.BitmapMask(this, this.baddy);
-    this.staticTile.setAlpha(0.55);
 
     // Audio
     this.ost = this.sound.add('ost-loop', {
@@ -42,16 +42,6 @@ class GameScene extends Scene {
     this.player.body.setBounce(0.2);
     this.player.body.setCollideWorldBounds(true);
     this.player.body.setGravityY(400);
-    
-    this.anims.create({
-      key:'left',
-    });
-    this.anims.create({
-      key:'right',
-    });
-    this.anims.create({
-      key:'up'
-    });
 
     const gfx = this.add.graphics();
     gfx.lineStyle(2, 0xFF0000, 1);
@@ -64,11 +54,33 @@ class GameScene extends Scene {
     this.physics.world.setBounds(0, 0, window.innerWidth, window.innerHeight / 2 + 100);
 
     // Start spawn clock
+    let speed = 100;
+
     this.time.addEvent({
       callback: () => {
-        console.log('spawn');
+        const x = this.player.x + 300;
+        const brick = this.physics.add.sprite(x, window.innerHeight / 2 - 100, 'static');
+        const mass = pMath.FloatBetween(0.25, 0.8);
+
+        brick.play('static-flicker');
+        brick.body.setCollideWorldBounds(true);
+        brick.body.setVelocityX(-speed);
+        brick.body.setBounce(mass, mass);
+        brick.setScale(1, 0.9 + (mass * 2));
+
+        this.physics.add.overlap(this.player, brick, (p: any, b: any) => {
+          if (p.y < b.y + b.displayHeight * 0.33) {
+            b.body.setCollideWorldBounds(false);
+            b.setAlpha(0.5);
+            p.setVelocityY(-200);
+          }
+        });
+
+        this.activeBricks.push(brick);
+
+        speed *= 1.05;
       },
-      delay: 1000,
+      delay: 4000,
       repeat: -1
     });
   }
@@ -80,8 +92,19 @@ class GameScene extends Scene {
 
     // Player jumping
     if (this.cursors.up.isDown && this.player.body.blocked.down) {
-      this.player.body.setVelocityY(-600);
+      this.player.body.setVelocityY(-500);
     }
+
+    // Brick cleanup
+    this.activeBricks.forEach((brick, i) => {
+      if (
+        brick.y > window.innerHeight + brick.displayHeight ||
+        brick.x <= this.baddy.x
+      ) {
+        brick.destroy();
+        this.activeBricks.splice(i, 1);
+      }
+    });
   }
 }
 
